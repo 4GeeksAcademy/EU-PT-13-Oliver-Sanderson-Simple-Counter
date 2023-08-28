2c1b44492a9bbfbeb0209ecc5cf96fc6b5364c8c
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2023-08-28 19:27:52 +0000

    working with count

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SecondCounter from "./secondCounter";
+import BetterCounter from "./betterCounter";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
 			<SecondCounter />
+			<BetterCounter />
 		</div>
 	);
 };
