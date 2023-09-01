3909c8d85c39d7a940532a5152ea0433ec9789c1
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2023-09-01 17:45:13 +0000

    improved look and leading zeros

--- src/js/component/secondCounter.jsx
+++ src/js/component/secondCounter.jsx
@@ -3,27 +3,44 @@ import Card from "./card";
 
 function SecondCounter() {
   const [counter, setCounter] = useState(0);
-  const [test, setTest] = useState(<Card displaySec={"0"} />)
-  let secondsArray = []
+  const [test, setTest] = useState(
+    <React.Fragment>
+      <Card displaySec={"0"} />
+      <Card displaySec={"0"} />
+      <Card displaySec={"0"} />
+      <Card displaySec={"0"} />
+      <Card displaySec={"0"} />
+      <Card displaySec={"0"} />
+    </React.Fragment>
+  )
+  let secondsArray = ["000000"]
+
+  const leadingZeros = (numberString) => {
+    let newNumberString = numberString
+    while (newNumberString.length < 6) {
+      newNumberString = "0" + newNumberString
+    }
+    return newNumberString
+  }
 
 
-  useEffect (() => {setInterval(() => {
-    setCounter(prevCount => { secondsArray = (prevCount + 1).toString().split(''); return prevCount + 1;});
-    // secondsArray = counter.toString().split('');      <------ WHY DID THIS NOT WORK? 'counter' SHOWS ZERO!
-    setTest(secondsArray.map((item) => {return (<Card displaySec={item} />)}))
-    console.log(test)
-    console.log(secondsArray)
-  }, 1000);}, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(prevCount => { secondsArray = leadingZeros((prevCount + 1).toString()).split(''); return prevCount + 1; });
+      // secondsArray = counter.toString().split('');      <------ WHY DID THIS NOT WORK? 'counter' SHOWS ZERO!
+      setTest(secondsArray.map((item) => { return (<Card displaySec={item} />) }))
+    }, 1000); return () => {
+      return (clearInterval(interval))}; // important to clearInterval to stop the interval once component is unmounted
+  }, []);
 
- 
+
 
   return (
     <div className="bg-dark text-light d-flex justify-content-center">
-      <div className="card bg-dark border-secondary numberCard m-3 pt-5 fs-1"><i class="fa-solid fa-clock"></i></div>
+      <div className="card bg-dark border-secondary numberCard m-3 pt-5 fs-1"><i class="fa-regular fa-clock"></i></div>
 
-      {/* <h2>Counting: {counter}</h2> */}
       {test}
-     
+
     </div>
   );
 }
